f65b3bc667421fe6ac2bfd3ca80e6652f4341f89
Ayush Kumar <kumarayush0427@gmail.com>
2023-09-19 00:03:09 +0530

    comparisons

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -29,5 +29,38 @@ let booleanIsLoggedIn= Boolean(isLoggedIn)
 let someNumber=33
 
 let stringNumber = String(someNumber)
-console.log(stringNumber)
-console.log(typeof stringNumber)
+// console.log(stringNumber)
+// console.log(typeof stringNumber)
+
+/*after conversion to string*/
+
+//  33 => "33"
+
+
+/******************************* Operations ***************************************/
+
+let value=3
+let negvalue= -value
+// console.log(negvalue)
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**3)
+// console.log(2/3)
+// console.log(2%3)
+
+let str1="hello"
+let str2=" ayush"
+
+let str3= str1+str2
+// console.log(str3)
+
+
+// console.log("1" + 2)
+// console.log(1 + "2")
+// console.log("1" + 2 + 2)
+// console.log(1+ 2 + "2")
+
+
+
--- 01_basics/04_comparison.js
+++ 01_basics/04_comparison.js
@@ -9,10 +9,18 @@
 // console.log("2">1)
 // console.log("02">1)
 
-console.log(null>0)
-console.log(null>=0)
-console.log(null==0)
+// console.log(null>0)
+// console.log(null==0)
+// console.log(null>=0)
 
 /*
-the reason is that an equality check == and comparisons > < >= <=
+the reason is that an equality check == and comparisons > < >= <= work differently. 
+conversion convert null to a number, treating it as 0.
+thats why null>=0 is true and null>0 is false.
+AND same for undefined
 */ 
+
+/* === */
+
+console.log("2"===2) 
+//it checks both the value and datatype
