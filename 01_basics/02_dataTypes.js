b22fff0a7a564628edb3d1156953af14384c5947
Ayush Kumar <kumarayush0427@gmail.com>
2023-09-20 17:23:23 +0530

    datatypes

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -19,8 +19,49 @@ let gender= "male"
 //  object
 
 
-console.log(typeof "yush");
-console.log(typeof age);
-console.log(typeof null);       //object
-console.log(typeof undefined);  //undefined
 
+// console.log(typeof "yush");
+// console.log(typeof age);
+// console.log(typeof null);       //object
+// console.log(typeof undefined);  //undefined
+
+
+
+/************************************** Summary ***************************************/
+
+//JavaScript is a dynamically typed language, which means that data types of variables are determined by the value they hold at runtime 
+//and can change throughout the program as we assign different values to them.
+
+
+//Primitive datatypes
+
+// 7 types: String, symbol, boolean, bigInt, number, null, undefined
+
+
+// Non-Primitive datatypes (Reference)
+
+// Array, objects, functions
+
+
+
+//how to use symbol
+
+// const id= Symbol('123')
+// const anotherId= Symbol('123')
+
+// console.log(id===anotherId)
+
+
+//non-primitive
+//array
+const heros=["xyz", "abc", "uio"]
+
+//objects
+let myObj={
+    name:"ayush",
+    age: 22
+}
+
+const myFunction= function(){
+    console.log("hello world")
+}
