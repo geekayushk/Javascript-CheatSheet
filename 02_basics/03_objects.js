97d750b8856288b35b82b704b486f9d61879f121
Ayush Kumar <kumarayush0427@gmail.com>
2023-09-30 12:01:56 +0530

    objects

--- 02_basics/03_objects.js
+++ 02_basics/03_objects.js
@@ -15,10 +15,21 @@ const JsUser = {
   lastLoginDays: ["Monday", "Saturday"],
 };
 
-console.log(JsUser.email);
-console.log(JsUser["email"]); // another method to access the value of the key
-console.log(JsUser[mySym]); //to access the value of symbol
-console.log(typeof mySym);
+// console.log(JsUser.email);
+// console.log(JsUser["email"]);    // another method to access the value of the key
+// console.log(JsUser[mySym]);     //to access the value of symbol
+// console.log(typeof mySym);
 
 JsUser.email = "ayushless@yahoo.com";
-Object.freeze(JsUser); //to freeze the
+// Object.freeze(JsUser); //to freeze or lock the values. so that it cant be modified.
+JsUser.email = "shanu@mail.com";
+// console.log(JsUser);
+
+JsUser.greeting = function () {
+  console.log("Hello js user");
+};
+JsUser.greetingTwo = function () {
+  console.log(`Hello js user, ${this.name}`);
+};
+console.log(JsUser.greeting());
+console.log(JsUser.greetingTwo());
