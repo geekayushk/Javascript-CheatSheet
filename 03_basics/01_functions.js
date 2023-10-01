db02d2d850c90f7f004eb5784b84b0db88c8c555
Ayush Kumar <kumarayush0427@gmail.com>
2023-10-01 23:15:08 +0530

    functions

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -26,7 +26,7 @@ const result = addTwoNumbers(3, 4);
 
 function loginUserMessage(username) {
   if (!username) {
-    console.log("please enter a username")
+    // console.log("please enter a username")
     return
   }
   return `${username} just logged in`
@@ -39,6 +39,44 @@ function loginUserMessage(username) {
 //   return `${username} just logged in`
 // }
 
-console.log(loginUserMessage())
+// console.log(loginUserMessage())
 
 
+function calculateCartPrice(...num1) {
+  return num1
+}
+// function calculateCartPrice(val1, val2, ...num1) {
+//   return num1
+// }
+//  ... this is also called as rest operator.
+//Based on thier usage it is called as a rest and spread operator
+//Rest opeator puts the value into an array. 
+//we use rest operator when we dont know how many arguments will be passed into the function so that we can perform operation on this values when it in array
+
+// console.log(calculateCartPrice(200, 400, 500, 2000))
+
+
+const user = {
+  username: "ayush",
+  price: 200
+}
+
+function handleObject(anyobject) {
+  console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+}
+
+// handleObject(user)
+
+handleObject({
+  username: "sam",
+  price: 399
+})
+
+const myNewArray = [200, 400, 100, 600]
+
+function returnSecondValue(getArray) {
+  return getArray[1]
+}
+
+// console.log(returnSecondValue(myNewArray))
+console.log(returnSecondValue([200, 400, 100, 600]))
